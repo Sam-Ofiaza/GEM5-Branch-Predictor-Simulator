293d34d577f1057aa5b6b2f53fd2c4462ec9cee5
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-03 18:20:48 -0500

    Change line numbers to match updated GEM5 source files in AWS EC2 Ubuntu instance

--- script.py
+++ script.py
@@ -26,17 +26,17 @@ TOURNY_GLOBAL_PRED_SIZES = [4096, 8192]
 TOURNEY_CHOICE_PRED_SIZES = [4096, 8192]
 
 BP_TYPES = {
-    50: lambda x: f'    branchPred = Param.BranchPredictor({x}, "Branch Predictor")'
+    40: lambda x: f'    branchPred = Param.BranchPredictor({x}, "Branch Predictor")'
 }
 
 BP_PARAMS = {
-    39: lambda x: f'    BTBEntries = Param.Unsigned({x}, "Number of BTB entries")\n',
-    50: lambda x: f'    localPredictorSize = Param.Unsigned({x}, "Size of local predictor")\n',
-    73: lambda x: f'    globalPredictorSize = Param.Unsigned({x}, "Size of global predictor")\n',
-    75: lambda x: f'    choicePredictorSize = Param.Unsigned({x}, "Size of choice predictor")\n',
-    59: lambda x: f'    localPredictorSize = Param.Unsigned({x}, "Size of local predictor")\n',
-    62: lambda x: f'    globalPredictorSize = Param.Unsigned({x}, "Size of global predictor")\n',
-    64: lambda x: f'    choicePredictorSize = Param.Unsigned({x}, "Size of choice predictor")\n',
+    65: lambda x: f'    BTBEntries = Param.Unsigned({x}, "Number of BTB entries")\n',
+    81: lambda x: f'    localPredictorSize = Param.Unsigned({x}, "Size of local predictor")\n',
+    104: lambda x: f'    globalPredictorSize = Param.Unsigned({x}, "Size of global predictor")\n',
+    106: lambda x: f'    choicePredictorSize = Param.Unsigned({x}, "Size of choice predictor")\n',
+    90: lambda x: f'    localPredictorSize = Param.Unsigned({x}, "Size of local predictor")\n',
+    93: lambda x: f'    globalPredictorSize = Param.Unsigned({x}, "Size of global predictor")\n',
+    95: lambda x: f'    choicePredictorSize = Param.Unsigned({x}, "Size of choice predictor")\n',
 }
 
 
@@ -51,28 +51,28 @@ def modify_file(filepath, callback):
 
 
 def change_bp_type(data, name=None):
-    data[50] = BP_TYPES[50](name)
+    data[40] = BP_TYPES[40](name)
     # print(name, end=' ')
 
 
 def change_local_bp_config(data, btb_entries=None, local_pred_size=None):
-    data[39] = BP_PARAMS[39](btb_entries)
-    data[50] = BP_PARAMS[50](local_pred_size)
+    data[65] = BP_PARAMS[65](btb_entries)
+    data[81] = BP_PARAMS[81](local_pred_size)
     # print(f'{btb_entries=} {local_pred_size=}')
 
 
 def change_bimode_bp_config(data, btb_entries=None, global_pred_size=None, choice_pred_size=None):
-    data[39] = BP_PARAMS[39](btb_entries)
-    data[73] = BP_PARAMS[73](global_pred_size)
-    data[75] = BP_PARAMS[75](choice_pred_size)
+    data[65] = BP_PARAMS[65](btb_entries)
+    data[104] = BP_PARAMS[104](global_pred_size)
+    data[106] = BP_PARAMS[106](choice_pred_size)
     # print(f'{btb_entries=} {global_pred_size=} {choice_pred_size=}')
 
 
 def change_tourny_bp_config(data, btb_entries=None, local_pred_size=None, global_pred_size=None, choice_pred_size=None):
-    data[39] = BP_PARAMS[39](btb_entries)
-    data[59] = BP_PARAMS[59](local_pred_size)
-    data[62] = BP_PARAMS[62](global_pred_size)
-    data[64] = BP_PARAMS[64](choice_pred_size)
+    data[65] = BP_PARAMS[65](btb_entries)
+    data[90] = BP_PARAMS[90](local_pred_size)
+    data[93] = BP_PARAMS[93](global_pred_size)
+    data[95] = BP_PARAMS[95](choice_pred_size)
     # print(f'{btb_entries=} {local_pred_size=} {global_pred_size=} {choice_pred_size=}')
 
 
@@ -120,6 +120,6 @@ def main():
 
 
 if len(sys.argv) > 1:
-    test()
+    test()  # Enter any arguments to run a single test for Local_2k_BTB_Entries_1k_Pred_Size
 else:
-    main()
+    main()  # Enter no arguments to run all tests
