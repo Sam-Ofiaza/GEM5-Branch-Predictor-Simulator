2b2ee6e5954addebdb0876053255c3b5858fff22
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-03 18:44:52 -0500

    Change script.sh to use waits instead of && to enforce synchronous execution

--- script.sh
+++ script.sh
@@ -12,14 +12,22 @@ BENCHMARK_ARGS="-I 5000000 --cpu-type=timing --caches --l2cache --l1d_size=128kB
 
 start_time=`date +%s`
 
-mkdir -p $BUILD_PATH/$OUTPUT_DIR_NAME && cp -rf gem5 $BUILD_PATH/$OUTPUT_DIR_NAME && 
-mkdir -p $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng && 
-cd $GEM5_DIR && 
-rm -rf build/X86 && 
-scons ../$BUILD_PATH/$OUTPUT_DIR_NAME/build/X86/gem5.opt &>> ../$OUTPUT_PATH/$OUTPUT_DIR_NAME/build.log && 
-cd .. && 
-$BUILD_DIR/$OUTPUT_DIR_NAME/gem5/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $BUILD_DIR/$OUTPUT_DIR_NAME/gem5/configs/example/se.py -c $HMMER_BENCHMARK_PATH -o $HMMER_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer.log && 
-$BUILD_DIR/$OUTPUT_DIR_NAME/gem5/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng $GEM5_DIR/configs/example/se.py -c $SJENG_BENCHMARK_PATH -o $SJENG_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng.log && 
+mkdir -p $BUILD_PATH/$OUTPUT_DIR_NAME && cp -rf gem5 $BUILD_PATH/$OUTPUT_DIR_NAME
+wait
+mkdir -p $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng
+wait
+cd $GEM5_DIR
+wait
+rm -rf build/X86
+wait 
+scons ../$BUILD_PATH/$OUTPUT_DIR_NAME/build/X86/gem5.opt &>> ../$OUTPUT_PATH/$OUTPUT_DIR_NAME/build.log
+wait
+cd ..
+wait
+$BUILD_DIR/$OUTPUT_DIR_NAME/gem5/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $BUILD_DIR/$OUTPUT_DIR_NAME/gem5/configs/example/se.py -c $HMMER_BENCHMARK_PATH -o $HMMER_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer.log
+wait
+$BUILD_DIR/$OUTPUT_DIR_NAME/gem5/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng $GEM5_DIR/configs/example/se.py -c $SJENG_BENCHMARK_PATH -o $SJENG_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng.log
+wait
 rm -rf $BUILD_PATH/$OUTPUT_DIR_NAME
 
 # mkdir -p $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng && cd $GEM5_DIR && rm -rf build/X86 && scons build/X86/gem5.opt -j4 &>> ../$OUTPUT_PATH/$OUTPUT_DIR_NAME/build.log && cd .. && $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $GEM5_DIR/configs/example/se.py -c $HMMER_BENCHMARK_PATH -o $HMMER_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer.log && $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng $GEM5_DIR/configs/example/se.py -c $SJENG_BENCHMARK_PATH -o $SJENG_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng.log
