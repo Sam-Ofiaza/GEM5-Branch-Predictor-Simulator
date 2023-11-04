eb8308b53f1dc732c4220fa9132f521d0584d4c0
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-04 18:46:21 -0500

    Change test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
+OUTPUT_DIR_NAME=$1
+GEM5_DIR=./gem5
+BUILD_PATH=./builds
+OUTPUT_PATH=./output
+HMMER_BENCHMARK_PATH=./Project1_SPEC/"456.hmmer"/src/benchmark
+HMMER_ARG_PATH=./Project1_SPEC/"456.hmmer"/data/bombesin.hmm
+SJENG_BENCHMARK_PATH=./Project1_SPEC/"458.sjeng"/src/benchmark
+SJENG_ARG_PATH=./Project1_SPEC/"458.sjeng"/data/test.txt
+BENCHMARK_ARGS="-I 5000000 --cpu-type=timing --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=1MB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 --cacheline_size=64"
+
+start_time=`date +%s`
+
 $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer $GEM5_DIR/configs/example/se.py -c $HMMER_BENCHMARK_PATH -o $HMMER_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/hmmer.log
 $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng $GEM5_DIR/configs/example/se.py -c $SJENG_BENCHMARK_PATH -o $SJENG_ARG_PATH $BENCHMARK_ARGS &>> $OUTPUT_PATH/$OUTPUT_DIR_NAME/sjeng.log
+
+echo $(expr `date +%s` - $start_time) s
