c884c5e74572449c640245f28a0db5d36d613849
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-05 04:53:40 -0600

    Fix collect_data.py to not return in get_PREDICTOR_data() fns

--- collect_data.py
+++ collect_data.py
@@ -19,7 +19,7 @@ TOURNEY_CHOICE_PRED_SIZES = ['4k', '8k']
 
 
 def get_stats(benchmark, stats_path):
-    print(stats_path)
+    # print(stats_path)
     with open(stats_path, 'r', encoding='utf-8') as file:
         file_data = file.readlines()
         btb_miss_pct = float(file_data[26].split()[1]) - 99
@@ -28,28 +28,31 @@ def get_stats(benchmark, stats_path):
         return (btb_miss_pct, branch_mispred_pct)
 
 
-def get_local_data(benchmark, x):
+def get_local_data(data, benchmark, x):
     for y in LOCAL_PRED_SIZES:
         stats_path = f'./output/Local_{x}_BTB_{y}_Pred/{benchmark}/stats.txt'
         stats_data = get_stats(benchmark, stats_path)
-        return [benchmark, 'Local', REVERSE_OUTPUT_DIR_NAME_MAP[x], REVERSE_OUTPUT_DIR_NAME_MAP[y], None, None, stats_data[0], stats_data[1]]
+        data.append([benchmark, 'Local', REVERSE_OUTPUT_DIR_NAME_MAP[x],
+                    REVERSE_OUTPUT_DIR_NAME_MAP[y], None, None, stats_data[0], stats_data[1]])
 
 
-def get_bimode_data(benchmark, x):
+def get_bimode_data(data, benchmark, x):
     for y in BIMODE_GLOBAL_PRED_SIZES:
         for z in BIMODE_CHOICE_PREDICTOR_SIZES:
             stats_path = f'./output/BiMode_{x}_BTB_{y}_Global_{z}_Choice/{benchmark}/stats.txt'
             stats_data = get_stats(benchmark, stats_path)
-            return [benchmark, 'BiMode', REVERSE_OUTPUT_DIR_NAME_MAP[x], REVERSE_OUTPUT_DIR_NAME_MAP[y], REVERSE_OUTPUT_DIR_NAME_MAP[z], None, stats_data[0], stats_data[1]]
+            data.append([benchmark, 'BiMode', REVERSE_OUTPUT_DIR_NAME_MAP[x], REVERSE_OUTPUT_DIR_NAME_MAP[y],
+                        REVERSE_OUTPUT_DIR_NAME_MAP[z], None, stats_data[0], stats_data[1]])
 
 
-def get_tournament_data(benchmark, x):
+def get_tournament_data(data, benchmark, x):
     for y in TOURNY_LOCAL_PRED_SIZES:
         for z in TOURNY_GLOBAL_PRED_SIZES:
             for k in TOURNEY_CHOICE_PRED_SIZES:
                 stats_path = f'./output/Tournament_{x}_BTB_{y}_Local_{z}_Global_{z}_Choice/{benchmark}/stats.txt'
                 stats_data = get_stats(benchmark, stats_path)
-                return [benchmark, 'Tournament', REVERSE_OUTPUT_DIR_NAME_MAP[x], REVERSE_OUTPUT_DIR_NAME_MAP[y], REVERSE_OUTPUT_DIR_NAME_MAP[z], REVERSE_OUTPUT_DIR_NAME_MAP[k], stats_data[0], stats_data[1]]
+                data.append([benchmark, 'Tournament', REVERSE_OUTPUT_DIR_NAME_MAP[x], REVERSE_OUTPUT_DIR_NAME_MAP[y],
+                            REVERSE_OUTPUT_DIR_NAME_MAP[z], REVERSE_OUTPUT_DIR_NAME_MAP[k], stats_data[0], stats_data[1]])
 
 
 data = []
@@ -59,11 +62,11 @@ for benchmark in ['hmmer', 'sjeng']:
         for x in BTB_ENTRIES:
             match bp_type:
                 case 'Local':
-                    data.append(get_local_data(benchmark, x))
+                    get_local_data(data, benchmark, x)
                 case 'BiMode':
-                    data.append(get_bimode_data(benchmark, x))
+                    get_bimode_data(data, benchmark, x)
                 case 'Tournament':
-                    data.append(get_tournament_data(benchmark, x))
+                    get_tournament_data(data, benchmark, x)
 
 df = pd.DataFrame(data, columns=['Benchmark', 'Branch Predictor', 'BTB Entries', 'Local Predictor Size',
                   'Global Predictor Size', 'Choice Predictor Size', 'BTB Miss %', 'Branch Misprediction %'])
