07d9da3104dcacf0d80d810d2bf78d50c05fe148
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-23 20:54:01 -0600

    Fix collect_data.py to extract the correct values

--- Caches/scripts/collect_data.py
+++ Caches/scripts/collect_data.py
@@ -43,25 +43,28 @@ params = {
 
 data = []
 
-for key, val in params.items():
-    cur_val = val["range"][0]
-    while cur_val <= val["range"][1]:
-        for benchmark in ['hmmer', 'sjeng']:
+for benchmark in ['hmmer', 'sjeng']:
+    for key, val in params.items():
+        cur_val = val["range"][0]
+        while cur_val <= val["range"][1]:
             stats_path = f'{OUTPUT_DIR}/{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}/{benchmark}/stats.txt'
             with open(stats_path, 'r', encoding='utf-8') as file:
-                file_data = file.readlines()
-                l1i_miss_rate = float(
-                    file_data[345 if benchmark == 'hmmer' else 329].split()[1])
-                l1d_miss_rate = float(
-                    file_data[135 if benchmark == 'hmmer' else 135].split()[1])
-                l2_miss_rate = float(
-                    file_data[475 if benchmark == 'hmmer' else 457].split()[1])
-                data.append(
-                    [benchmark, key, f'{cur_val}{"_" if val["units"] else ""}{val["units"]}', l1i_miss_rate, l1d_miss_rate, l2_miss_rate])
+                l1i_miss_rate, l1d_miss_rate, l2_miss_rate, = -1, -1, -1
 
+                for idx, line in enumerate(file):
+                    if 'system.cpu.dcache.overallMissRate::total' in line:
+                        l1d_miss_rate = float(line.split()[1])
+                    elif 'system.cpu.icache.overallMissRate::total' in line:
+                        l1i_miss_rate = float(line.split()[1])
+                    elif 'system.l2.overallMissRate::total' in line:
+                        l2_miss_rate = float(line.split()[1])
+                        break
+
+                data.append(
+                    [benchmark, f'{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}', l1i_miss_rate, l1d_miss_rate, l2_miss_rate])
             cur_val *= 2
 
-df = pd.DataFrame(data, columns=['Benchmark', 'Parameter',
-                  'Value', 'L1I Miss Rate', 'L1D Miss Rate', "L2 Miss Rate"])
+df = pd.DataFrame(data, columns=[
+                  'Benchmark', 'Parameter', 'L1I Miss Rate', 'L1D Miss Rate', "L2 Miss Rate"])
 
-df.to_excel(OUTPUT_FILE)
+df.to_excel(f'{OUTPUT_DIR}/{OUTPUT_FILE}')
