8da6618218b603a60ccff135272f46f9dc297820
Sam Ofiaza <sam.ofiaza@gmail.com>
2023-11-28 16:07:04 -0600

    Add cpu type parameter to script.py and cost function to collect_data.py

--- Caches/scripts/collect_data.py
+++ Caches/scripts/collect_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 OUTPUT_DIR = '../output'
 OUTPUT_FILE = 'data.xlsx'
@@ -7,78 +8,123 @@ params = {
     "L1I_Size": {
         "units": "KB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "L1D_Size": {
         "units": "KB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "L2_Size": {
         "units": "MB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "Cacheline": {
         "units": "bytes",
         "range": (8, 512),
-        "default": 64
+        "default": 8
     },
     "L1I_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
     },
     "L1D_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
     },
     "L2_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
+    },
+}
+
+cpu_types = [
+    {'param': 'BaseSimpleCPU', 'name': 'Base'},
+    {'param': 'TimingSimpleCPU', 'name': 'Timing'},
+    {'param': 'AtomicSimpleCPU', 'name': 'Atomic'},
+]
+
+cost_config = {
+    'cpu_type': {'Base': 1, 'Timing': 1.5, 'Atomic': 1.5},
+    "L1I_Size": {
+        'cost': lambda x: x,
+        'weight': 3,
+    },
+    "L1D_Size": {
+        'cost': lambda x: x,
+        'weight': 3,
+    },
+    "L2_Size": {
+        'cost': lambda x: x,
+        'weight': 1,
+    },
+    "Cacheline": {
+        'cost': lambda x: math.log2(x) - 2,
+        'weight': 2,
+    },
+    "L1I_Assoc": {
+        'cost': lambda x: x,
+        'weight': 2.5,
+    },
+    "L1D_Assoc": {
+        'cost': lambda x: x,
+        'weight': 2.5,
+    },
+    "L2_Assoc": {
+        'cost': lambda x: x,
+        'weight': 1.5,
     },
 }
 
 data = []
 
 for benchmark in ['hmmer', 'sjeng']:
-    for key, val in params.items():
-        cur_val = val["range"][0]
-        while cur_val <= val["range"][1]:
-            stats_path = f'{OUTPUT_DIR}/{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}/{benchmark}/stats.txt'
-            with open(stats_path, 'r', encoding='utf-8') as file:
-                direct_cpi, l1i_miss_rate, l1d_miss_rate, l2_miss_rate = -1, -1, -1, -1
-                l1i_miss_ticks, l1d_miss_ticks, l2_miss_ticks = -1, -1, -1
+    for cpu_type in cpu_types:
+        for key, val in params.items():
+            cur_val = val["range"][0]
+            while cur_val <= val["range"][1]:
+                cost = 0
+                for key2, val2 in params.items():
+                    cost += cost_config[key2]['cost'](
+                        cur_val if key == key2 else params[key2]['default']) * cost_config[key2]['weight']
+                cost *= cost_config['cpu_type'][cpu_type['name']]
 
-                for idx, line in enumerate(file):
-                    if 'system.cpu.dcache.overallMissLatency::total' in line:
-                        l1d_miss_ticks = float(line.split()[1])
-                    elif 'system.cpu.icache.overallMissLatency::total' in line:
-                        l1i_miss_rate = float(line.split()[1])
-                    elif 'system.l2.overallMissLatency::total' in line:
-                        l2_miss_rate = float(line.split()[1])
-                    elif 'system.cpu.cpi' in line:
-                        direct_cpi = float(line.split()[1])
-                    elif 'system.cpu.dcache.overallMissRate::total' in line:
-                        l1d_miss_rate = float(line.split()[1])
-                    elif 'system.cpu.icache.overallMissRate::total' in line:
-                        l1i_miss_rate = float(line.split()[1])
-                    elif 'system.l2.overallMissRate::total' in line:
-                        l2_miss_rate = float(line.split()[1])
-                        break
+                stats_path = f'{OUTPUT_DIR}/{cpu_type["name"]}_{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}/{benchmark}/stats.txt'
+                with open(stats_path, 'r', encoding='utf-8') as file:
+                    direct_cpi, l1i_miss_rate, l1d_miss_rate, l2_miss_rate = -1, -1, -1, -1
+                    l1i_miss_ticks, l1d_miss_ticks, l2_miss_ticks = -1, -1, -1
 
-                miss_rate_cpi = 1 + l1i_miss_rate + l1d_miss_rate + l2_miss_rate
-                miss_ticks_cpi = 1 + \
-                    (((l1i_miss_ticks / 1000) + (l1d_miss_ticks / 1000) +
-                     (l2_miss_ticks / 1000)) / 50000000)
+                    for idx, line in enumerate(file):
+                        if 'system.cpu.dcache.overallMissLatency::total' in line:
+                            l1d_miss_ticks = float(line.split()[1])
+                        elif 'system.cpu.icache.overallMissLatency::total' in line:
+                            l1i_miss_ticks = float(line.split()[1])
+                        elif 'system.l2.overallMissLatency::total' in line:
+                            l2_miss_ticks = float(line.split()[1])
+                        elif 'system.cpu.cpi' in line:
+                            direct_cpi = float(line.split()[1])
+                        elif 'system.cpu.dcache.overallMissRate::total' in line:
+                            l1d_miss_rate = float(line.split()[1])
+                        elif 'system.cpu.icache.overallMissRate::total' in line:
+                            l1i_miss_rate = float(line.split()[1])
+                        elif 'system.l2.overallMissRate::total' in line:
+                            l2_miss_rate = float(line.split()[1])
+                            break
 
-                data.append(
-                    [benchmark, f'{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}', l1i_miss_rate, l1d_miss_rate, l2_miss_rate, direct_cpi, miss_rate_cpi, miss_ticks_cpi])
-            cur_val *= 2
+                    miss_rate_cpi = 1 + l1i_miss_rate + l1d_miss_rate + l2_miss_rate
+                    miss_ticks_cpi = 1 + \
+                        (((l1i_miss_ticks / 1000) + (l1d_miss_ticks / 1000) +
+                          (l2_miss_ticks / 1000)) / 50000000)
+
+                    data.append(
+                        [benchmark, f'{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}', direct_cpi, miss_rate_cpi, miss_ticks_cpi, cost])
+                cur_val *= 2
 
 df = pd.DataFrame(data, columns=[
-                  'Benchmark', 'Parameter', 'L1I Miss Rate', 'L1D Miss Rate', "L2 Miss Rate", "Direct CPI", "CPI using Miss Rate", "CPI using Miss Ticks"])
+                  'Benchmark', 'Parameter', "Direct CPI", "CPI using Miss Rate", "CPI using Miss Ticks", "Cost"])
 
 df.to_excel(f'{OUTPUT_DIR}/{OUTPUT_FILE}')
--- Caches/scripts/script.py
+++ Caches/scripts/script.py
@@ -9,47 +9,53 @@ params = {
     "L1I_Size": {
         "units": "KB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "L1D_Size": {
         "units": "KB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "L2_Size": {
         "units": "MB",
         "range": (1, 128),
-        "default": 8
+        "default": 1
     },
     "Cacheline": {
         "units": "bytes",
         "range": (8, 512),
-        "default": 64
+        "default": 8
     },
     "L1I_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
     },
     "L1D_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
     },
     "L2_Assoc": {
         "units": "",
         "range": (1, 8),
-        "default": 2
+        "default": 1
     },
 }
+
+cpu_types = [
+    {'param': 'BaseSimpleCPU', 'name': 'Base'},
+    {'param': 'TimingSimpleCPU', 'name': 'Timing'},
+    {'param': 'AtomicSimpleCPU', 'name': 'Atomic'},
+]
 
 cur_params = {}
 for key, val in params.items():
     cur_params[key] = val["default"]
 
 
-def build_benchmark_args(L1I_Size=params["L1I_Size"]["default"], L1D_Size=params["L1D_Size"]["default"], L2_Size=params["L2_Size"]["default"], Cacheline=params["Cacheline"]["default"], L1I_Assoc=params["L1I_Assoc"]["default"], L1D_Assoc=params["L1D_Assoc"]["default"], L2_Assoc=params["L2_Assoc"]["default"]):
-    return f'-I 50000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size={L1D_Size}kB --l1i_size={L1I_Size}kB --l2_size={L2_Size}MB --l1d_assoc={L1D_Assoc} --l1i_assoc={L1I_Assoc} --l2_assoc={L2_Assoc} --cacheline_size={Cacheline}'
+def build_benchmark_args(Cpu_Type=params["Cpu_Type"], L1I_Size=params["L1I_Size"]["default"], L1D_Size=params["L1D_Size"]["default"], L2_Size=params["L2_Size"]["default"], Cacheline=params["Cacheline"]["default"], L1I_Assoc=params["L1I_Assoc"]["default"], L1D_Assoc=params["L1D_Assoc"]["default"], L2_Assoc=params["L2_Assoc"]["default"]):
+    return f'-I 50000000 --cpu-type={Cpu_Type} --caches --l2cache --l1d_size={L1D_Size}kB --l1i_size={L1I_Size}kB --l2_size={L2_Size}MB --l1d_assoc={L1D_Assoc} --l1i_assoc={L1I_Assoc} --l2_assoc={L2_Assoc} --cacheline_size={Cacheline}'
 
 
 # # Set the simulator branch predictor to NULL
@@ -68,22 +74,28 @@ subprocess.call(['./compile.sh', sys.argv[1],
 # Run the benchmarks for each configuration in parallel
 pool = mp.Pool()
 
-for key, val in params.items():
-    cur_val = val["range"][0]
-    while cur_val <= val["range"][1]:
-        cur_params[key] = cur_val
-        output_dir_name = f'{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}'
-        benchmark_args = build_benchmark_args(**cur_params)
+for cpu_type in cpu_types:
+    cur_params["Cpu_Type"] = cpu_type['param']
 
-        # print(f'{cur_params=}')
-        # print(f'{output_dir_name=}')
-        # print(f'{benchmark_args=}')
+    pool.apply_async(subprocess.Popen, args=(
+        ['./run_benchmarks.sh', f'{cpu_type["name"]}_No_Caches', f'-I 50000000 --cpu-type={cpu_type["name"]}', '&>>', f'{LOGS_DIR}/{cpu_type["name"]}_No_Caches.log'], ))
 
-        pool.apply_async(subprocess.Popen, args=(
-            ['./run_benchmarks.sh', output_dir_name, benchmark_args, '&>>', f'{LOGS_DIR}/{output_dir_name}.log'], ))
+    for key, val in params.items():
+        cur_val = val["range"][0]
+        while cur_val <= val["range"][1]:
+            cur_params[key] = cur_val
+            output_dir_name = f'{cpu_type["name"]}_{key}_{cur_val}{"_" if val["units"] else ""}{val["units"]}'
+            benchmark_args = build_benchmark_args(**cur_params)
 
-        cur_val *= 2
-    cur_params[key] = params[key]["default"]
+            # print(f'{cur_params=}')
+            # print(f'{output_dir_name=}')
+            # print(f'{benchmark_args=}')
+
+            pool.apply_async(subprocess.Popen, args=(
+                ['./run_benchmarks.sh', output_dir_name, benchmark_args, '&>>', f'{LOGS_DIR}/{output_dir_name}.log'], ))
+
+            cur_val *= 2
+        cur_params[key] = params[key]["default"]
 
 pool.close()
 pool.join()
